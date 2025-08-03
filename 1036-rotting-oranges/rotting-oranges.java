54a2808f23294434f7dff1c7650e73099dfcaa09
sadhanamodhave92 <43167556+sadhanamodhave92@users.noreply.github.com>
2025-08-03 12:59:48 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (75.56%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,49 +2,43 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int r= grid.length;
         int c= grid[0].length;
-        int fresh=0, rotten=0;
+        int fresh=0;
        
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(grid[i][j]==2){
-                    rotten++;
-                    q.add(new Pair<>(i, j));
+                    q.add(new int[]{i, j});
                 }
                 if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
-        if(fresh==0){
-            return 0;
-        }
+        if(fresh==0) return 0;
+
         int[] dx= {1,0,-1,0};
         int[] dy= {0,1,0,-1};
         int min =0;
+
         while(!q.isEmpty()){
-            int size = q.size();
+            int size = q.size(); // ✅ fix
+            boolean changed = false;
             for(int i=0;i<size;i++){
-                   Pair<Integer, Integer> pair = q.poll();
-                    for(int j=0;j<4;j++){
-                        int x=pair.getKey()+dx[j];
-                        int y=pair.getValue()+dy[j];
-                        if(x>=0 && y>=0 && x<r && y<c && grid[x][y]==1){
-                            grid[x][y] =2;
-                            q.add(new Pair<>(x, y));
-                            fresh--;
-                        }
+                int[] curr = q.poll();
+                for(int j=0;j<4;j++){
+                    int x=curr[0]+dx[j];
+                    int y=curr[1]+dy[j];
+                    if(x>=0 && y>=0 && x<r && y<c && grid[x][y]==1){
+                        grid[x][y] =2;
+                        q.add(new int[]{x, y});
+                        fresh--;
+                        changed = true;
                     }
+                }
             }
-             if(q.size()>0){
-                min++;
-            }
-           
+            if(changed) min++;
         }
-        if(fresh==0){
-            return min;
-        }
-        return -1;
-        
+        return (fresh==0) ? min : -1;
     }
 }
