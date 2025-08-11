c9a327436da245440177818cac0190ec6b36bcf2
sadhanamodhave92 <43167556+sadhanamodhave92@users.noreply.github.com>
2025-08-11 15:42:33 +0530

    Time: 3 ms (100.00%) | Memory: 93.4 MB (16.13%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -1,24 +1,31 @@
 class Solution {
-    private static final int MOD=1000000007;
+
+    private static final int MOD = 1000000007;
+
     public int[] productQueries(int n, int[][] queries) {
-        List<Integer> powerSumArray= new ArrayList<>();
-        int rep=1;
-        while(n>0){
-            if(n%2==1){
-                powerSumArray.add(rep);
+        List<Integer> bins = new ArrayList<>();
+        int rep = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                bins.add(rep);
             }
-            n/=2;
-            rep*=2;
+            n /= 2;
+            rep *= 2;
         }
-        int[] ans=new int[queries.length];
-        for(int i=0;i<queries.length;i++){
-            long cur=1;
-            int start = queries[i][0];
-            int end = queries[i][1];
-            for(int j=start;j<=end;j++){
-                cur=(cur*powerSumArray.get(j))%MOD;
+
+        int m = bins.size();
+        int[][] results = new int[m][m];
+        for (int i = 0; i < m; i++) {
+            long cur = 1;
+            for (int j = i; j < m; j++) {
+                cur = (cur * bins.get(j)) % MOD;
+                results[i][j] = (int) cur;
             }
-            ans[i]=(int)cur;
+        }
+
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            ans[i] = results[queries[i][0]][queries[i][1]];
         }
         return ans;
     }
