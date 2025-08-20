4455fb093d6b127c0efba53fe1e01ed18be9c141
sadhanamodhave92 <43167556+sadhanamodhave92@users.noreply.github.com>
2025-08-20 16:52:31 +0530

    Time: 6 ms (75.00%) | Memory: 54.6 MB (98.84%) - LeetSync

---
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp =new int[m+1][n+1];
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==1){
+                    dp[i+1][j+1] = Math.min(
+                        Math.min(dp[i][j],dp[i+1][j]),dp[i][j+1]) +1;
+                        ans+=dp[i+1][j+1];
+
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
